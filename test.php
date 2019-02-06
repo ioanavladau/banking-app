d0dcb681a925b8c0523636e52d8261871dee4ed2
Ioana Vladau <ioan0414@stud.kea.dk>
2019-02-06 11:34:08 +0100

    unique ids in the database

--- test.php
+++ test.php
@@ -18,3 +18,19 @@ foreach($jData->clients as $jClient){
   echo "<p>last name: $jClient->lastName</p>";
   // echo '<h2>Yes</h2>';
 }
+
+$sNewClient = '{}';
+$jNewClient = json_decode($sNewClient);
+$jNewClient->id = uniqid();
+$jNewClient->name = "AAA";
+$jNewClient->lastName = "BBB";
+
+// put the new client inside the array of clients
+// all this is in RAM
+array_push($jData->clients, $jNewClient);
+// Did I save the JSON object back to the file? NO
+$sFinalData = json_encode($jData);
+// save it
+file_put_contents('database.txt', $sFinalData);
+
+echo json_encode($jNewClient); // object to text
