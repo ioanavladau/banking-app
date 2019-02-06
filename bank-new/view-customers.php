24ffc0e75b858d4191ace69735594419c8782fbb
Ioana Vladau <ioan0414@stud.kea.dk>
2019-02-06 23:41:56 +0100

    display customers + saving in the database.txt working

--- bank-new/view-customers.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$sData = file_get_contents("database.txt");
-$jData = json_decode($sData); // convert text into a JSON object
-
-foreach($jData->clients as $jClient){
-  echo "<p>id: $jClient->id</p>";
-  echo "<p>name: $jClient->name</p>";
-}
